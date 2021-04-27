4b93c5c3ee7f0fdfe0ea56aebb9124b471f53858
Leon <grunbergerleon@web.de>
2021-04-27 17:24:35 +0200

    Login & Register
    Einbauen der Light Version

--- lernplattform/routes/web.php
+++ lernplattform/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('{any}', function () {
+/*Route::get('{any}', function () {
     return view('app');
-})->where('any', '.*');
+})->where('any', '.*'); */
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
