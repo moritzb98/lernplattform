aef0a64ff6a1692e3a668b41c29a31f810e431ab
Moritz <moritz.banhardt@web.de>
2021-04-27 21:31:48 +0200

    fix auth

--- lernplattform/routes/web.php
+++ lernplattform/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('{any}', function () {
+Route::get('{any}', function () {
     return view('app');
-})->where('any', '.*'); */
+})->where('any', '.*');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
