4ed64124eaab868a9891568871e5ee0e8aad3009
Theresa <theresabrenner@gmx.net>
2021-05-27 22:17:24 +0200

    Auto stash before merge of "main" and "origin/main"

--- lernplattform/resources/js/views/Quiz.vue
+++ lernplattform/resources/js/views/Quiz.vue
@@ -15,7 +15,8 @@
                 <span class="material-icons-outlined icon-für-oberen-Bereich">biotech</span>
                 <div class="überschrift-oberer-Bereich" >
                     {{ quizName }}
-                    <div v-if="started" class="unterüberschrift-oberer-bereich">Frage {{ currentQuestion }}/{{ questions.length }}</div>
+                    <div v-if="started && !summary" class="unterüberschrift-oberer-bereich">Frage {{ currentQuestion }}/{{ questions.length }}</div>
+                    <div v-else-if="summary" class="unterüberschrift-oberer-bereich">Zusammenfassung</div>
                     <div v-else class="unterüberschrift-oberer-bereich">Noch nicht begonnen</div>
                 </div>
             </div>
@@ -26,7 +27,13 @@
 
         <!-- Content -->
         <!-- start quiz -->
-        <p v-if="!started" @click="startQuiz()">Starte das Quiz</p>
+        <div v-if="!started" class="fragen-container">
+            <button @click="startQuiz()" class="mdc-button mdc-button--raised button--big button--small abstand-weg">
+                <p class="button-text no-margin">Starte das Quiz</p>
+                <span class="material-icons-outlined">play_circle</span>
+            </button>
+        </div>
+
 
         <!-- Questions -->
         <div v-if="!summary" class="fragen-container-forLoop-0height" v-for="question, index in questions" :key="question.id">
@@ -36,25 +43,31 @@
                 </div>
                 <div v-for="answer, index in question.answers" :key="answer.id">
                     <div v-if="!answered" class="antwort-container" @click="sendAnswer(answer, question.answers, question)">
-                        <p class="antwort-optionen-buchstaben" :class='"answer"+answer.id'>
-                            A
+                        <p class="antwort-optionen-buchstaben" :class='"answer"+answer.id' :id='"answerletter"+answer.id'>
+                            <span v-if="index===0">A</span>
+                            <span v-if="index===1">B</span>
+                            <span v-if="index===2">C</span>
+                            <span v-if="index===3">D</span>
                         </p>
-                        <p class="antwort-optionen" :class='"answer"+answer.id'>
-                            Antwort {{index + 1}}: {{answer.answer}}
+                        <p class="antwort-optionen" :class='"answer"+answer.id' :id='"answerid"+answer.id'>
+                            {{answer.answer}}
                         </p>
                     </div>
                     <div v-else class="antwort-container">
-                        <p class="antwort-optionen-buchstaben" :class='"answer"+answer.id'>
-                            A
+                        <p class="antwort-optionen-buchstaben" :class='"answer"+answer.id' :id='"answerletter"+answer.id'>
+                            <span v-if="index===0">A</span>
+                            <span v-if="index===1">B</span>
+                            <span v-if="index===2">C</span>
+                            <span v-if="index===3">D</span>
                         </p>
-                        <p class="antwort-optionen" :class='"answer"+answer.id'>
-                            Antwort {{index + 1}}: {{answer.answer}}
+                        <p class="antwort-optionen" :class='"answer"+answer.id' :id='"answerid"+answer.id'>
+                            {{answer.answer}}
                         </p>
                     </div>
                 </div>
                 <div v-if="!answered && !noNextQuestion" @click="skipQuestion(question)" class="überspringen">überspringen</div>
-                <div v-else-if="answered && !noNextQuestion" @click="nextQuestion()" class="überspringen">Nächste Frage</div>
-                <div v-else @click="showSummary(question)" class="überspringen">Ergebnisse</div>
+                <div v-else-if="answered && !noNextQuestion" @click="nextQuestion()" class="überspringen überspringen--fett">Nächste Frage</div>
+                <div v-else @click="showSummary(question)" class="überspringen überspringen--fett">Ergebnisse</div>
             </div>
             <br>
         </div>
@@ -131,37 +144,35 @@
                 // set answer status to true -> user cannot answer anymore
                 this.answered = true;
                 // get clicked DOM-Elements
-                let clickedAnswers = document.getElementsByClassName('answer'+answer.id);
-
-
+                let clickedAnswerId = document.getElementById('answerid'+answer.id);
+                let clickedAnswerLetter = document.getElementById('answerletter'+answer.id);
 
                 // check answer
                 if(answer.is_correct){
                     // color green if answer was right
-                    for(var i=0; i<clickedAnswers.length; i++){
-                        clickedAnswers[i].classList.add('correct');
-                    };
+                    clickedAnswerId.classList.add('correct');
+                    clickedAnswerLetter.classList.add('correct-letter');
 
                     // Push the answer to user correct answers
                     this.userAnswers.correct.push(question);
                 }else{
                     // color red if answer was wrong
-                    for(var i=0; i<clickedAnswers.length; i++){
-                        clickedAnswers[i].classList.add('wrong');
-                    };
+                    clickedAnswerId.classList.add('wrong');
+                    clickedAnswerLetter.classList.add('wrong-letter');
 
                     // search for correct answers and get DOM-Elements
-                    let correctAnswers;
+                    let correctAnswerId;
+                    let correctAnswerLetter;
                     for(var i =0; i<allAnswers.length; i++){
                         if(allAnswers[i].is_correct){
-                            correctAnswers = document.getElementsByClassName('answer'+allAnswers[i].id);
+                            correctAnswerId = document.getElementById('answerid'+allAnswers[i].id);
+                            correctAnswerLetter = document.getElementById('answerletter'+allAnswers[i].id);
                         }
                     }
 
                     // color correct answer green
-                    for(var i=0; i<correctAnswers.length; i++){
-                        correctAnswers[i].classList.add('correct');
-                    };
+                    correctAnswerId.classList.add('correct');
+                    correctAnswerLetter.classList.add('correct-letter');
 
                     // Push the answer to user wrong answers
                     this.userAnswers.wrong.push(question);
@@ -240,6 +251,7 @@
         display: flex;
         flex-wrap: nowrap;
         position: relative;
+        margin-bottom: 10px;
     }
 
     .fragen-container-forLoop-0height{
@@ -259,6 +271,7 @@
         z-index: 2;
         font-size: 1.5em;
         font-weight: bold;
+        margin: auto 0;
     }
 
     .antwort-optionen{
@@ -275,6 +288,9 @@
         background-image: radial-gradient(white, white), radial-gradient(circle at top left,white,#ED553B);
         background-origin: border-box;
         background-clip: content-box, border-box;
+        top: 50%;
+        transform: translate(0, -50%);
+        line-height: 25px;
     }
 
     .frage{
@@ -290,11 +306,30 @@
     }
 
     .correct{
-        background: green;
+        background: #C3F2D1;
+        border: solid 2px #5BD96A;
+    }
+
+    .correct-letter{
+        background: #5BD96A;
     }
 
     .wrong{
-        background: red;
+        background: #FAC4C5;
+        border: solid 2px #DF6A6A;
+    }
+
+    .wrong-letter{
+        background: #DF6A6A;
+    }
+
+    .no-margin{
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+
+    .überspringen--fett{
+        font-weight: bold;
     }
 
 
