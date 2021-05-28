a3aefb4550662974250da0e43646ba1a572745b9
Leon <grunbergerleon@web.de>
2021-05-28 10:14:52 +0200

    Erweiterte Suche

--- lernplattform/app/Http/Controllers/SearchFileController.php
+++ lernplattform/app/Http/Controllers/SearchFileController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\FileUpload;
+use App\Models\Room;
+use App\Models\Collection;
+use App\Models\Quiz;
+use App\Models\Karteikartenset;
+use App\Models\User;
 use Spatie\Searchable\Search;
 use Auth;
 use App\Http\Resources\UserFilesCollection;
@@ -13,7 +18,8 @@ class SearchFileController extends Controller
 {
         public function index(Request $request){
 
-            $results = (new Search())->registerModel(FileUpload::class, ['displayname'])
+            $results = (new Search())->registerModel(FileUpload::class, ['displayname'])->registerModel(Quiz::class, ['name'])->registerModel(Room::class, ['name'])
+            ->registerModel(Collection::class, ['name'])->registerModel(User::class, ['name'])
             ->search($request->input('query'));
 
 
--- lernplattform/app/Models/Quiz.php
+++ lernplattform/app/Models/Quiz.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Searchable\Searchable;
+use Spatie\Searchable\Searchresult;
 
-class Quiz extends Model
+class Quiz extends Model implements Searchable
 {
     use HasFactory;
 
@@ -17,5 +19,11 @@ class Quiz extends Model
 
     public function category(){
         return $this->belongsTo('App\Models\Category', 'category_id', 'id');
+    }
+
+    public function getSearchResult(): SearchResult {
+        $url = '/spa/quiz/'.$this->name;
+
+        return new SearchResult($this, $this->name, $url);
     }
 }
